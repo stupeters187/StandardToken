239a11cd85c48f7d6b56404778ea65a0d227b054
Stu Peters <stu_peters187@hotmail.com>
2017-01-17 13:53:20 -0500

    define constructor function

--- contracts/StandardToken.sol
+++ contracts/StandardToken.sol
@@ -1,7 +1,10 @@
 contract StandardToken {
+  
+  address public owner;
   mapping (address => uint) balances;
 
   function StandardToken() {
-    
+    owner = msg.sender;
+    balances[owner] = 1000;
   }
 }

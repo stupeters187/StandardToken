6533fc7bc8bcce9e02cd0685a10df45679f42cc7
Stu Peters <stu_peters187@hotmail.com>
2017-01-17 13:58:25 -0500

    getBalance function

--- contracts/StandardToken.sol
+++ contracts/StandardToken.sol
@@ -7,4 +7,8 @@ contract StandardToken {
     owner = msg.sender;
     balances[owner] = 1000;
   }
+
+  function getBalance(address _user) constant returns (uint _balance) {
+    return balances[_user];
+  }
 }

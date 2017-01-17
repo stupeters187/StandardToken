2892d967ef3e7080b67aa21237eadbcfb7dfde7d
Stu Peters <stu_peters187@hotmail.com>
2017-01-17 14:10:31 -0500

    transfer function

--- contracts/StandardToken.sol
+++ contracts/StandardToken.sol
@@ -8,6 +8,17 @@ contract StandardToken {
     balances[owner] = 1000;
   }
 
+  function transfer(address _to, uint _value) return (bool success) {
+    if (balances[msg.sender] < _value) {
+      return false
+    }
+
+
+    balances[msg.sender] -= _value;
+    balances[_to] += _value;
+    return true;
+  }
+
   function getBalance(address _user) constant returns (uint _balance) {
     return balances[_user];
   }
